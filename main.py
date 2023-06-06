1c1ca916ad0fcd20918015053d7c22f28b2e5fa0
Red-Thirten <rehlmgaming@gmail.com>
2023-06-05 18:44:25 -0700

    [v2.1.5] Fix Various Runtime Crashes (#6)
    - Refactored bot subclass to be more organized/proper with static methods.
    - Refactored ServerStatus cog to have more explicit typing in functions.
    - Removed `set_top_player` function and moved its functionality to the `record_player_stats` function.
    - Added `PlayerStatsTextChannelID` to config check routine.
    - Removed setting stats channel description in the `on_ready` function because it was causing issues if Bot got disconnected from Discord. It was set anyways during the loop.
    - `StatusLoop` now checks for "real" games that have 2 or more players before recording stats. This also fixes the top player runtime crash.
    - Added top player and end-game time to stats saved message.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """main.py
 
 Main file to start Backstab
-Date: 06/01/2023
+Date: 06/05/2023
 Authors: David Wolfe (Red-Thirten)
 Licensed under GNU GPLv3 - See LICENSE for more details.
 """
@@ -13,7 +13,7 @@ from discord.ext import commands
 from src import BackstabBot
 
 def main():
-    VERSION = "2.1.4"
+    VERSION = "2.1.5"
     AUTHORS = "Red-Thirten"
     COGS_LIST = [
         "CogServerStatus"
