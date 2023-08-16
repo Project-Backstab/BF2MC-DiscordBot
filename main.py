a0f0b7d827360ed8d119ae08d4c93b6c71b0a575
Red-Thirten <rehlmgaming@gmail.com>
2023-08-16 10:52:11 -0700

    Update version number for release

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """main.py
 
 Main file to start Backstab
-Date: 08/14/2023
+Date: 08/16/2023
 Authors: David Wolfe (Red-Thirten)
 Licensed under GNU GPLv3 - See LICENSE for more details.
 """
@@ -13,7 +13,7 @@ from discord.ext import commands
 from src import BackstabBot
 
 def main():
-    VERSION = "2.5.4"
+    VERSION = "2.5.5"
     AUTHORS = "Red-Thirten"
     COGS_LIST = [
         "CogPlayerStats",
